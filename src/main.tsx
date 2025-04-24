c807734a8213c941a20b91006bc852df23bcb96a
Gabriel Duarte <gabriel.d.c.s.master@gmail.com>
2025-04-24 16:59:12 -0300

    feat(p1): cria componente principal e faz hello

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
+import 'primeflex/primeflex.css'; // Estilos do PrimeFlex
+import 'primereact/resources/themes/saga-blue/theme.css'; // Tema PrimeReact
+import 'primereact/resources/primereact.min.css'; // Estilos principais
+import 'primeicons/primeicons.css'; // Ícones PrimeIcons
+import './index.css'; // Seus estilos globais
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
